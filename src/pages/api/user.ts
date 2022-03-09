648d4eb5fe9ef0a675e56aa8be9fb814a33e718a
Maelyly <maelyalways@gmail.com>
2022-03-09 17:50:06 -0300

    sing up done

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -1,10 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import { UserRepository } from "../../repositories/user.repository"
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method === "POST") {
     const userRepository = new UserRepository()
-    res.json(userRepository.createUser(req.body))
+    let newuser = await userRepository.createUser(req.body)
+    console.log(newuser)
+    res.json(newuser)
   } else {
     res.status(200).json({ body: "get" })
   }
