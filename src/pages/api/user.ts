cdf4218d3f2fe99b1622b8fd2cfa6f02fc2dde05
Sabrina <sabrinasantana0190@gmail.com>
2022-03-15 17:50:14 -0300

    fix: user api

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -1,14 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import { UserService } from "../../services/user.service"
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  
-  const userService = new UserService()
-
-  if (req.method === "POST") {
-    res.json(await userService.createUser(req.body))
-  
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'POST') {
+    const userService = new UserService()
+    const { email, password, name } = req.body;
+    let newuser = await userService.createUser({ email, password, name });
+    res.json(newuser);
   } else {
     res.status(200).json({ body: "get" })
   }
+
 }
