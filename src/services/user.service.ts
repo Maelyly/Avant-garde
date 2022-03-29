3bf676ba126bdf50e72ae2d513b8bf2af12a2b51
Sabrina <sabrinasantana0190@gmail.com>
2022-03-28 23:58:38 -0300

    back login

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,10 +1,29 @@
-import { IUser } from "../@types/user"
-import { UserRepository } from "../repositories/user.repository"
+import { compareSync, hashSync } from 'bcrypt';
+import { ILogin, IUser } from '../@types/user';
+import { UserRepository } from '../repositories/user.repository';
 
 export class UserService {
   private userRepository = new UserRepository();
 
-  public createUser = (user: IUser) => {
-    return this.userRepository.createUser(user)
-  }
+  public createUser = async (user: IUser) => {
+    const existentUser = await this.userRepository.findByEmail(user.email);
+
+    if (existentUser) {
+      return {
+        error: 'email already taken'
+      };
+    }
+    return this.userRepository.createUser(user);
+  };
+
+  public authenticate = async (login: ILogin) => {
+    const user = await this.userRepository.findByEmail(login.email);
+    if (!user) return null;
+
+    const hashPassword = hashSync(login.password, 10);
+
+    const isAuthenticated = compareSync(hashPassword, user?.password);
+
+    return isAuthenticated ? user : null;
+  };
 }
