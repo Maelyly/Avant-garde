f3c4ad36a0fd6fad8e107e232f8e6f7a0b2962a4
Maelyly <maelyalways@gmail.com>
2022-04-19 22:29:00 -0300

    filter add

--- src/pages/api/card/[index].tsx
+++ src/pages/api/card/[index].tsx
@@ -27,7 +27,9 @@ export default async function handler(
     });
     res.json(newCard);
   } else if (req.method === 'GET') {
+    const data = req.query;
     const cards = await cardService.findAllByUser(user.id);
+    const cardsFilter = await cardService.filter(data);
 
     return res.status(200).json(cards);
   } else {
